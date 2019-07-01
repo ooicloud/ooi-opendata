6d5b3b8a51ca17da8af350f091991b8591359f03
Tim Crone <tjcrone@gmail.com>
2019-07-01 14:49:41 -0400

    Restructure the ooiod package

--- ooiod/__init__.py
+++ ooiod/__init__.py
@@ -1,1 +1,12 @@
-from .ooiod import get_keys
+"""
+OOIOD
+=====
+Provides
+  1. Tools for authenticating with Azure and moving data from OOI into Azure Open Datasets
+
+Available subpackages
+---------------------
+secrets
+    Functions for working with the secrets files and authentication.
+"""
+from . import secrets
